3871e50744de4f96ff573f8e33d0cabe74ab00e3
Peter Njenga <pn@sky.garden>
2018-09-14 01:39:42 +0300

    - Prepare for search - Create a mixin for users

---
+++ common/mixins.py
@@ -0,0 +1,45 @@
+import operator
+from django.db.models import Q
+class GetQuerysetBaseMixin(object):
+    def get_queryset(self, *args, **kwargs):
+        self.queryset = super(GetQuerysetBaseMixin, self).get_queryset(*args, **kwargs)
+
+        query = self.request.GET.get("q") or False
+        # An icontains search using values in search fields specified in the calling view
+
+
+        if query and hasattr(self, 'search_fields'):
+            fields = [(field + '__icontains', query) for field in self.search_fields]
+
+            q_list = [Q(x) for x in fields]
+
+            self.queryset = self.queryset.filter(
+                reduce(operator.or_, q_list)
+            )
+
+            self.queryset = self.queryset.distinct()
+
+        return self.queryset
+
+class GetQuerysetMixin(GetQuerysetBaseMixin):
+    user = None
+
+    def filter_by_shopper(self):
+        """
+            Tries to get the institution to which a given object belongs
+        """
+        return self.queryset.filter(
+            shopper=self.request.user
+        ).exclude(
+            shopper=None
+        )
+
+
+    def get_queryset(self, *args, **kwargs):
+        self.queryset = super(GetQuerysetMixin, self).get_queryset(*args, **kwargs)
+
+        self.user = self.request.user
+
+        self.queryset = self.filter_by_shopper()
+
+        return  self.queryset
